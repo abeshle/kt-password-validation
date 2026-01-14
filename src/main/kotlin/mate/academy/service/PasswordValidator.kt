489303b961f53c1c0a35f41516d69b2ac7ce49c7
Anastasiia Beshleha <anastasiiabeshleha@gmail.com>
2026-01-13 22:46:26 -0400

    password validation mini app

--- src/main/kotlin/mate/academy/service/PasswordValidator.kt
+++ src/main/kotlin/mate/academy/service/PasswordValidator.kt
@@ -1,8 +1,13 @@
 package mate.academy.service
 
+import mate.academy.exception.PasswordValidationException
+
+const val SIZE = 10
 // This class will validate password requirements
 class PasswordValidator {
     fun validate(password: String, repeatPassword: String) {
-        // write your code here
+        if(password.length < SIZE || password!= repeatPassword) {
+            throw PasswordValidationException("Wrong passwords")
+        }
     }
 }
